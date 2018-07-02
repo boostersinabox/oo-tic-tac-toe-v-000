fa19ec828421a8bd02ea500d54b6a4e340590fd8
Stacy Carr <stacy@boostersinabox.com>
2018-07-02 17:42:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,12 +18,13 @@ WIN_COMBINATIONS = [
 ]
 
 # Display board
-def display_board
-  puts "   |   |   "
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
 
 
