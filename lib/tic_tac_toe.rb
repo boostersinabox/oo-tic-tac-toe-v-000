f629f8fb839fb7a578e3c054751e62df5fd75486
Stacy Carr <stacy@boostersinabox.com>
2018-07-02 19:54:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -72,22 +72,6 @@ else
   display_board
 end
 
-# turn count - board.count{|token| token == "X" || token == "O"}
-def turn_count
-  turns = 0
-  board.each do |token|
-    if token == "X" || token == "O"
-      turns += 1
-    end
-     end
-  turns
-end
-
-# current player 
-def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
-end
-
 #Won? 
   def won?
     WIN_COMBINATIONS.any? do |combo|
